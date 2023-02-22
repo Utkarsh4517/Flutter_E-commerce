c1920230de032231a00be5ae7f4557de3df84d04
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-02-22 23:32:18 +0530

    created mongodb server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,46 @@
-console.log("Hello,world");
+//console.log("Hello,world");
 
 // creating an api using express js
 // we can name this const express as const xyz
-const express = require('express');
+
+// IMPORTS FROM PACKAGES
+
+const express = require("express");
+const mongoose = require("mongoose");
 // similar to import line in dart ^
+
+//IMPORTS FROM OTHER FILES
+
+const authRouter = require("./routes/auth");
+
+// INIT
 const PORT = 3000;
 // initialising express and saving it in the app variable.
 const app = express();
 
+//middleware
+app.use(authRouter);
+
+// connections
+mongoose
+  .connect()
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 // CREATING API
 // GET,PUT,POST,DELETE,UPDATE -> CRUD
 
 // GET -- fetching some data
-http://<youripaddress>/hello-world
+//<youripaddress>/hello-world
 // app.get('/path', () {}) // ipaddress/path
-app.get('/hello-world', (req,res) => {
-    res.send('hello world');
-} )
+http: app.get("/hello-world", (req, res) => {
+  res.json({ hi: "hello , world" });
+});
 
 app.listen(PORT, () => {
-    console.log('connected at port ' + PORT + ' hello') ;
-} );
+  console.log("connected at port " + PORT + " hello");
+});
+// time 1:10:23
---
+++ server/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const authRouter = express.Router();
+
+authRouter.post('/api/signup', (req, res) => {
+    // get the data from client
+
+    const {name,email,password} =  req.body;
+    
+
+
+    // post that data in database
+    // return that data to the user
+})
+
+module.exports = authRouter; // like static, or export var in py
